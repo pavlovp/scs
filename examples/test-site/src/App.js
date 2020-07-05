4c7829610fc974e54e519b33b9604ad890d41279
Pavel Pavlov <pavel.pavlov1990@gmail.com>
2020-07-05 18:29:38 +0200

    trigger deploy

--- examples/test-site/src/App.js
+++ examples/test-site/src/App.js
@@ -6,9 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test site
         </p>
         <a
           className="App-link"
@@ -16,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
